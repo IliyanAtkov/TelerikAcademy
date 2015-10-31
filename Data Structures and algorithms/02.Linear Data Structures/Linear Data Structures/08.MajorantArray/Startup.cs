af2045fff1ab70b90ae631a8e977ac25e6ae1e60
Producenta <producenta@gmail.com>
2015-10-31 17:13:04 +0200

    Added 8 task from Data structures and algorithms

---
+++ Data Structures and algorithms/02.Linear Data Structures/Linear Data Structures/08.MajorantArray/Startup.cs
@@ -0,0 +1,42 @@
+﻿namespace MajorantArray
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        static void Main()
+        {
+            int[] arr = { 2, 2, 3, 3, 2, 3, 4, 3, 3 };
+            var timesOfOccOfNumber = new Dictionary<int, int>();
+            var majorantNumbers = new HashSet<int>();
+
+            int numberOfOcc = arr.Length / 2 + 1;
+
+            foreach (var number in arr)
+            {
+                if (timesOfOccOfNumber.ContainsKey(number))
+                {
+                    timesOfOccOfNumber[number] += 1;
+                    if (timesOfOccOfNumber[number] == numberOfOcc)
+                    {
+                        majorantNumbers.Add(number);
+                    }
+                    else if (timesOfOccOfNumber[number] > numberOfOcc)
+                    {
+                        majorantNumbers.Remove(number);
+                    }
+                }
+                else
+                {
+                    timesOfOccOfNumber[number] = 1;
+                }
+            }
+
+            foreach (var majorantNumber in majorantNumbers)
+            {
+                Console.WriteLine("Majorant number {0} ", majorantNumber);
+            }
+        }
+    }
+}
