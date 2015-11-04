a07756b41302e0d58f64e180c0b33eb952c8c1bf
Producenta <producenta@gmail.com>
2015-11-05 00:32:58 +0200

    Dictinaries,HashTables and Sets homework 1,2 task

---
+++ Data Structures and algorithms/04.Dictionaries, Hash Tables and Sets/DictionariesHashTAndSets/01.NumberOfOccurance/Startup.cs
@@ -0,0 +1,32 @@
+﻿namespace NumberOfOccurance
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            double[] numbers = { 3, 4, 4, -2.5, 3, 3, 4, 3, -2.5 };
+            var numberOfOccurances = new Dictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (numberOfOccurances.ContainsKey(number))
+                {
+                    numberOfOccurances[number] += 1;
+                }
+                else
+                {
+                    numberOfOccurances[number] = 1;
+                }
+            }
+
+            foreach (var occ in numberOfOccurances)
+            {
+                Console.WriteLine("{0} -> {1} times", occ.Key, occ.Value);
+            }
+
+        }
+    }
+}
---
+++ Data Structures and algorithms/04.Dictionaries, Hash Tables and Sets/DictionariesHashTAndSets/02.StringOddNumberOfTimes/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace StringOddNumberOfTimes
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            string[] words = { "C#", "SQL", "PHP", "PHP", "SQL", "SQL" };
+            var oddWordsOccurences = new Dictionary<string, int>();
+            foreach (var word in words)
+            {
+                if (oddWordsOccurences.ContainsKey(word))
+                {
+                    oddWordsOccurences[word] += 1;
+
+                    if (oddWordsOccurences[word] % 2 == 0)
+                    {
+                        oddWordsOccurences.Remove(word);
+                    }
+                }
+                else
+                {
+                    oddWordsOccurences[word] = 1;
+                }
+            }
+
+            Console.Write("Odd number of times occurs:");
+            foreach (var occ in oddWordsOccurences)
+            {
+                Console.Write(" {0},", occ.Key);
+            }
+            Console.WriteLine();
+
+            
+        }
+    }
+}
