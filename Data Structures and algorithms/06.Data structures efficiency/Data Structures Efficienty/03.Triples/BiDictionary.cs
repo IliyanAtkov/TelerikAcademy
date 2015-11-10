634a1e6b3ffa428a9989c400df317d194d81650d
Producenta <producenta@gmail.com>
2015-11-10 21:15:16 +0200

    Data structures efficienty homework ready added Third task

---
+++ Data Structures and algorithms/06.Data structures efficiency/Data Structures Efficienty/03.Triples/BiDictionary.cs
@@ -0,0 +1,71 @@
+﻿namespace Triples
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using Wintellect.PowerCollections;
+
+    public class BiDictionary<K1, K2, T>
+        where K1 : IComparable<K1> 
+        where K2 : IComparable<K2>
+    {
+        private OrderedMultiDictionary<K1, T> orderedCollectionByKey1;
+        private OrderedMultiDictionary<K2, T> orderedCollectionByKey2;
+        
+        public BiDictionary()
+        {
+            this.orderedCollectionByKey1 = new OrderedMultiDictionary<K1, T>(true);
+            this.orderedCollectionByKey2 = new OrderedMultiDictionary<K2, T>(true);
+        }
+
+        public void Add(K1 key1, K2 key2, T value)
+        {
+            this.orderedCollectionByKey1.Add(key1, value);
+            this.orderedCollectionByKey2.Add(key2, value);
+        }
+
+        public ICollection<T> Find(K1 key)
+        {
+            if (orderedCollectionByKey1.ContainsKey(key))
+            {
+                return orderedCollectionByKey1[key];
+            }
+
+            return null;
+        }
+
+        public ICollection<T> Find(K2 key)
+        {
+            if (orderedCollectionByKey2.ContainsKey(key))
+            {
+                return orderedCollectionByKey2[key];
+            }
+
+            return null;
+        }
+
+       public IEnumerable<T> Find(K1 key1, K2 key2)
+        {
+            if (orderedCollectionByKey1.ContainsKey(key1) && orderedCollectionByKey2.ContainsKey(key2))
+            {
+                return orderedCollectionByKey1[key1].Intersect(orderedCollectionByKey2[key2]);
+            }
+
+            return null;
+        }
+
+        public ICollection<T> Remove(K1 key1, K2 key2, T value)
+        {
+            bool foundFirst = orderedCollectionByKey1.ContainsKey(key1);
+            bool foundSecond = orderedCollectionByKey2.ContainsKey(key2);
+
+            if (foundFirst && foundSecond)
+            {
+                orderedCollectionByKey1.Remove(key1);
+                orderedCollectionByKey2.Remove(key2);
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Data Structures and algorithms/06.Data structures efficiency/Data Structures Efficienty/03.Triples/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace Triples
+{
+    using System;
+    using System.Linq;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var biDictionary = new BiDictionary<int, string, string>();
+            biDictionary.Add(5, "da", "Value as value");
+            biDictionary.Add(3, "ne", "Another value");
+            biDictionary.Add(3, "da", "HAHAHAHA");
+
+            Console.WriteLine(biDictionary.Find(3));
+            Console.WriteLine(biDictionary.Find("da"));
+            biDictionary.Find(3, "ne").ToList().ForEach(Console.WriteLine);
+
+        }
+    }
+}
